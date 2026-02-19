49e6785e4fdbbc7ca3e3eed99e67fab933815c0d
Taanveer22 <taanveer469@gmail.com>
2026-02-19 07:49:06 +0600

    alhamdulliah dot env file added

--- index.js
+++ index.js
@@ -6,10 +6,9 @@
 // 5. Routes
 // 6. Server Startup (app.listen)
 // ===========================================================
-// ZPlijzsirUGlVdpa
-// postmanTest
 
 // 1=> required
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -23,7 +22,7 @@ app.use(cors());
 app.use(
   cors({
     origin: "http://localhost:5173/",
-  })
+  }),
 );
 app.use(express.json());
 const logger = (req, res, next) => {
@@ -33,8 +32,8 @@ const logger = (req, res, next) => {
 
 // 4=> databse configuration
 
-const uri =
-  "mongodb+srv://postmanTest:ZPlijzsirUGlVdpa@cluster0.89rnkti.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -64,7 +63,7 @@ async function runMongoDB() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } catch (error) {
     console.log(error);
